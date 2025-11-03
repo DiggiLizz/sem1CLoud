dd061823cd5592a0abea03e9a2288d1e0ce46e05
Lilimon <lili.zapata@duocuc.cl>
2025-11-03 20:25:21 -0300

    Delete EjemploMicroservicio1 directory

--- EjemploMicroservicio1/src/main/java/cl/duoc/ejemplo/microservicio/repo/EventoRepository.java
+++
@@ -1,6 +0,0 @@
-package cl.duoc.ejemplo.microservicio.repo;
-
-import cl.duoc.ejemplo.microservicio.model.Evento;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EventoRepository extends JpaRepository<Evento, Long> {}
