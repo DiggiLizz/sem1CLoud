2a9d8f6aa06641cf49a84bb56fb158aa43eb2d92
Lilian Zapata <lili.zapata@duocuc.c>
2025-12-01 19:16:31 -0300

    cambio security config

--- src/main/java/cl/duoc/ejemplo/microservicio/config/SecurityConfig.java
+++ src/main/java/cl/duoc/ejemplo/microservicio/config/SecurityConfig.java
@@ -1,15 +1,22 @@
 package cl.duoc.ejemplo.microservicio.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtDecoders;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    // Tomamos el issuer que ya tienes configurado en application.properties
+    @Value("${spring.security.oauth2.resourceserver.jwt.issuer-uri}")
+    private String issuerUri;
 
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -25,4 +32,12 @@ public class SecurityConfig {
 
         return http.build();
     }
+
+    // Bean necesario para que Spring Security pueda validar el JWT
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        // Crea el decoder a partir del issuer (Azure AD en tu caso)
+        return JwtDecoders.fromIssuerLocation(issuerUri);
+    }
 }
+
